5b50f92157732c416b619cf33827d53ac5b29e9a
vusanim <vusani.mutshinya@sita.co.za>
2024-07-18 09:35:11 +0200

    C-k movement.

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -4,8 +4,8 @@ local opts = { noremap = true, silent = true }
 
 -- Normal mode mapping
 keymap('n', '<C-j>', ':m .+1<CR>==', opts)
-keymap('n', '<C-k>', ':m .-1<CR>==', opts)
+keymap('n', '<C-k>', ':m .-2<CR>==', opts)
 
 -- Visual mode mapping
 keymap('v', '<C-j>', ":m '>+1<CR>gv=gv", opts)
-keymap('v', '<C-k>', ":m '>-1<CR>gv=gv", opts)
+keymap('v', '<C-k>', ":m '<-2<CR>gv=gv", opts)
